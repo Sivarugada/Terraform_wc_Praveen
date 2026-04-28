dc024569c8bab92af6af59a3b1ea8437a3bcb10f
Rugada Sivasatya <91111607+Sivarugada@users.noreply.github.com>
2026-04-28 15:48:52 +0530

    Refactor AWS region and configure S3 backend
    Updated AWS region variable reference and added S3 backend configuration and region also

--- config.tf
+++ config.tf
@@ -1,3 +1,11 @@
 provider "aws" {
-    region = "${var.AWS_REGION}"
+    region = var.AWS_REGION
+}
+
+terraform {
+  backend "s3" {
+    bucket = "sivarugada-portifolio-2026"   # 👈 YOUR BUCKET NAME
+    key    = "terraform/"       # 👈 path inside bucket
+    region = "up-south-1"
+  }
 }
