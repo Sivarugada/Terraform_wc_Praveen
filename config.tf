388694e13cb6d6382420c768b5d332f1cd0d52a0
Rugada Sivasatya <91111607+Sivarugada@users.noreply.github.com>
2026-04-28 16:08:28 +0530

    Update config.tf
    when we mention folder path in config.tf folder dont start and end with forward slace

--- config.tf
+++ config.tf
@@ -5,7 +5,7 @@ provider "aws" {
 terraform {
   backend "s3" {
     bucket = "sivarugada-portifolio-2026"   # 👈 YOUR BUCKET NAME
-    key    = "terraform/"       # 👈 path inside bucket
+    key    = "terraform/terraform.tfstate"       # 👈 path inside bucket
     region = "up-south-1"
   }
 }
